0805365bf02f74768e0b27926d91d86ee4be170f
Leonhard Chen <chen.leonhard@gmail.com>
2026-08-30 01:01:49 +0200

    reduced filesize

--- Ubersreik5/scripts/mods/Ubersreik5/version.lua
+++ Ubersreik5/scripts/mods/Ubersreik5/version.lua
@@ -4,8 +4,8 @@ local WORKSHOP_ITEM_ID = "3790000790"
 
 -- Unix timestamp (UTC) + 1000s (buffer)
 -- Timestamp below serves as marker for "_Version Mod.bat"
--- 2026-08-29 23:12 UTC
-local OUR_VERSION_TIMESTAMP = 1788045147
+-- 2026-08-29 23:15 UTC
+local OUR_VERSION_TIMESTAMP = 1788045313
 
 mod.up_to_date_callbacks = {}
 
